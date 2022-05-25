46c088036d365b743566334283e0f2712fe84c84
sneidermendoza <88171078+sneidermendoza@users.noreply.github.com>
2022-05-25 11:26:56 -0500

    operadores_jerarquia
    operadores_jerarquia

---
+++ operadore_jerarquias.py
@@ -0,0 +1,13 @@
+### Jerarquias 
+# corchetes{} y parentecis()
+# potencia y raiz
+# multiplicacion y divicion
+# suma y resta###
+valor = 80 
+
+resultado1 = 50 / 3 + (28 - valor)
+print(resultado1)
+resultado2 = 19 + 40 / 5 - (20 * 3)
+print(resultado2)
+resultado3 = 12 + 80 * 3 - 10
+print(resultado3)
