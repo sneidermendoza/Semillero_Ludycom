27520b4eaeee75fdc9402f5586bf0e1375c372bb
sneidermendoza <88171078+sneidermendoza@users.noreply.github.com>
2022-05-25 09:39:45 -0500

    ejercicio _de_matrices
    ejercicio de array

---
+++ ejercicio_matrices.py
@@ -0,0 +1,13 @@
+
+paises = ["argentina", "autralia", "belgica", "brasil", "chile", "egipto", "francia", "panama", "sudafrica", "turquia", "tunez"]
+
+print(len(paises))
+print(paises[3])
+print(paises[0])
+#print(paises[-2]) este daria error por que no existe esta posicion
+#print(paìses[4]) esto daria error por que la valiable con tilde no existe
+#print(paises[15]) este daria error por que no existe esta posicion
+print(paises[8])
+#print(paises[13]) este daria error por que no existe esta posicion
+
+print("-".join(paises))
