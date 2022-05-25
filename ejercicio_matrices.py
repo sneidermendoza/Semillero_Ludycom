872eec34e3c9d1c697d4a31f18227305d4755a66
sneidermendoza <88171078+sneidermendoza@users.noreply.github.com>
2022-05-25 10:32:22 -0500

    matrices_colecciones_array
    se le coloco en comentario el tipo de datos y nuevo ejerccicio

--- ejercicio_matrices.py
+++ ejercicio_matrices.py
@@ -1,5 +1,6 @@
+#TUPLAS no se le puede añadir elementos o modificar
 
-paises = ["argentina", "autralia", "belgica", "brasil", "chile", "egipto", "francia", "panama", "sudafrica", "turquia", "tunez"]
+paises = ("argentina", "autralia", "belgica", "brasil", "chile", "egipto", "francia", "panama", "sudafrica", "turquia", "tunez")
 
 print(len(paises))
 print(paises[3])
@@ -11,3 +12,27 @@ print(paises[8])
 #print(paises[13]) este daria error por que no existe esta posicion
 
 print("-".join(paises))
+
+print("////////////////////////////////////////////////////////////////////////")
+
+
+# ARRAY se puede coleccionar diferentes tipos de datos y se puede modificar añadir o eliminar
+
+paises2 = ["panama", "egipto", "argentina"]
+
+paises2.append("francia")
+print(paises2)
+
+paises2.remove(paises2[1])
+print(paises2)
+
+paises2.append("australia")
+print(paises2)
+
+paises2.sort()
+print(paises2)
+
+paises2.append("brasil")
+print(paises2)
+
+
