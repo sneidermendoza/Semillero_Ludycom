b3f1d4ad48a60488f9d6f10b97f400d5020818d9
sneidermendoza <88171078+sneidermendoza@users.noreply.github.com>
2022-05-25 14:53:32 -0500

    estructuras_de_control
    estructura de control if y else

---
+++ estructuras_de_control.py
@@ -0,0 +1,59 @@
+if (5 > 5):
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+
+if (12 < 8):
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+
+if ("si" == "sì"):
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+
+if ("f" != "@"):
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+
+if True:
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+if False:
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+if(8 >= 8):
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+
+
+if (30.27 <= 50):
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+
+if ("a" == "a"):
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+
+if (50 != 50):
+    print("Entre en el if")
+else:
+    print("Entre en el else")
+
+
