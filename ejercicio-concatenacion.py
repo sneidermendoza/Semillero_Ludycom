e680527c0cac60b72a817c93dcf65da221dc3f2d
sneidermendoza <88171078+sneidermendoza@users.noreply.github.com>
2022-05-25 11:57:33 -0500

    ejercicio_de_concatenación
    concatenando variables

---
+++ ejercicio-concatenacion.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+day = 25
+month = 5
+year = 2022
+dt = datetime.now()
+hour = dt.hour
+minutes = dt.minute
+second = dt.second
+
+
+print(f"La fecha de hoy es {day}/{month}/{year}")
+
+
+print(f"El dia {day},del mes {month}, del año {year}, siendo la hora: {hour}:{minutes}:{second}, se realizo este codigo ")
+
