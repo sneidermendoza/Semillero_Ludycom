6c877e9ca3db15e4a871020c06fd8ffde9c0505b
sneidermendoza <88171078+sneidermendoza@users.noreply.github.com>
2022-05-26 14:48:20 -0500

    ejercicio_final_popularización
    calcular el área de un circulo o de un triangulo

---
+++ ejercicio_final_programacion_modular.py
@@ -0,0 +1,33 @@
+print("""
+Programa para calcular el area de una figura geometrica
+""")
+def calcular_area_circulo():
+        radio = float(input("Ingrese el radio: "))
+        area = (radio* radio) * 3.14159
+        print(f"El areaes: {area}")
+
+def calcular_area_triangulo():
+        base = float(input("Ingrese la medida de la base: "))
+        altura = float(input("Ingrese la medida de la altura: "))
+        area = (base* altura) / 2
+        print(f"El area es: {area}")
+
+opcion = int(input("Por favor seleccione una opcion:\n-> 1-(Circulo)\n-> 2-(Triangulo)\n="))
+
+while True:
+    if opcion == 1:
+        calcular_area_circulo()
+        break
+    elif opcion == 2:
+        calcular_area_triangulo()
+        break
+
+    else:
+        print("~~Opcion invalida elige de nuevo~~")
+        opcion = input("Por favor seleccione una opcion:\n-> 1-(Circulo)\n-> 2-(Triangulo)\n=")
+
+
+
+   
+    
+    
